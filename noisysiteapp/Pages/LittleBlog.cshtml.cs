ab083a71ac07b89dad70913f104a32cd4836992a
Andy Morris <noisyvolt@outlook.com>
2022-12-15 18:12:58 +0000

    hmm

---
+++ noisysiteapp/Pages/LittleBlog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+public class LittleBlogModel : PageModel
+{
+
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ noisysiteapp/Pages/Wander.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+public class WanderModel : PageModel
+{
+
+    public void OnGet()
+    {
+        
+    }
+}
